17b9423fd401ed18573f3b2156b9f73e2fc642c0
Alessandra <alessamica@gmail.com>
2021-07-05 21:49:06 -0400

    Filter data by year

--- balanceScale.r
+++ balanceScale.r
@@ -1,9 +1,20 @@
+library("dplyr")
+library("gapminder")
+
+head(gapminder)
+# Guardamos una busqueda filtrada por anio 2007 en una variable llamada gap_07
+gap_07 <-filter(gapminder, year== 2007)
+
+
+
+
+
 # 2. DESCRIPCIÓN Y EXPORACIÓN DE LOS DATOS
 
 #-------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
-# VARIABLES-
+# VARIABLES: 
 # Name of the class: 3(L,B,R) Left, Balanced, Right
 # Left Weight: 5(1,2,3,4,5)
 # Left Distance: 5(1,2,3,4,5)
