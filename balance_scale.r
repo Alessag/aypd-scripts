70903260d57ca475a765f95aa3b511515552e2ed
Alessandra <alessamica@gmail.com>
2021-07-08 15:36:37 -0400

    Generate table and plot results

--- balance_scale.r
+++ balance_scale.r
@@ -1,11 +1,33 @@
 # Cargar paquetes 
 library(readr)
+library(gapminder)
+
 
 # Buscar la ruta del archivo y guardarla en una variables
 ruta_csv <- file.choose()
 
-# Importar datos con gapminder
-data <- read.csv(ruta_csv)
+# Importar datos con gapminder y le asignamos un titulo a cada columna
+dataBalance <- read.csv(ruta_csv,
+                        col.names = c('color',
+                                      'size',
+                                      'act',
+                                      'age',
+                                      'inflated'))
+
+# DESCRIPCIÓN Y EXPORACIÓN DE LOS DATOS
+# VARIABLES: 
+# Color: 
+# Size 
+# Act 
+# Age 
+# Inflated
 
 # Mirar datos 
-head(data)
+head(dataBalance)
+
+#Generando la tabla agrupada
+table(dataBalance$color)
+
+#Graficar de barras basicas
+barplot(table(dataBalance$size))
+
