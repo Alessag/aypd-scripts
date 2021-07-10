de521a50383ea587c240843b282dbaa3287a2b48
Alessandra <alessamica@gmail.com>
2021-07-10 12:53:13 -0400

    Update balloons script

--- Categoricos/balloons.r
+++ Categoricos/balloons.r
@@ -1,12 +1,39 @@
-# Cargar paquetes 
+#### ---------------------------------------------------------------------------
+#### TEMA 1. Introducción al procesamiento de datos
+#### EXPLORACION Y DESCRIPCION DE DATOS CUANTITATIVOS
+
+#### REALIZADO POR: Alessandra Amicarella Girardi, V-26.209.731
+
+#### ---------------------------------------------------------------------------
+#### ---------------------------------------------------------------------------
+
+####                  CARGA DE PAQUETES
+ 
 library(readr)
 library(gapminder)
 
+#### ---------------------------------------------------------------------------
+#### ---------------------------------------------------------------------------
 
-# Buscar la ruta del archivo y guardarla en una variables
+# Lectura de datos: Archivo adult+stretch.data
+# La primera fila no contiene los nombres de las columnas
+# Los datos estan separados por ,
+# La primera columna identifica a las filas
+# El archivo contiene 20 filas
+# Las variables contienen informacion de globos estirados
+# Las variables registradas fueron: 
+# Color: Amaraillo, purpura
+# Size: Grande, pequeño
+# Act: Estirar, sumergir
+# Age: Adulto, niño
+# Inflated: Verdadero, falso
+
+
+# Buscamos la ruta del archivo y la guardamos en una variable
 ruta_csv <- file.choose()
 
-# Importar datos con gapminder y le asignamos un titulo a cada columna
+# Importar datos, los leemos, le asignamos un titulo a cada columna y los guardamos
+# en una variable llamada dataBalloons
 dataBalloons <- read.csv(ruta_csv,
                         col.names = c('color',
                                       'size',
@@ -14,18 +41,10 @@ dataBalloons <- read.csv(ruta_csv,
                                       'age',
                                       'inflated'))
 
-# DESCRIPCIÓN Y EXPORACIÓN DE LOS DATOS
-# VARIABLES: 
-# Color: yellow, purple
-# Size: large, small
-# Act: stretch, dip
-# Age: adult, child
-# Inflated: T, F
-
-# Mirar datos 
+# Miramos los datos 
 head(dataBalloons)
 
-#Generando la tabla agrupada
+#Generamos la tabla agrupada para la variable: Inflados
 table(dataBalloons$inflated)
 
 #Graficamos en un diagrama de barras basicas 
